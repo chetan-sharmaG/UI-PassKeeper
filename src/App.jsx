aa92a46a49b7d481e91e1dc385e27ff5980e4ddc
Chetan Sharma <cs194555@gmail.com>
2024-05-14 11:42:31 +0530

    enhancement3

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ function App() {
 
   return (
     <>
-      {/* <div className="absolute top-0 z-[-2]  w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"> */}
+      <div className="absolute top-0 z-[-2]  w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
         <Navbar />
-        {/* <div className='min-h-[85vh]'> <Manager /></div> */}
+        <div className='min-h-[85vh]'> <Manager /></div>
         <Footer />
-      {/* </div> */}
+      </div>
 
     </>
   )
