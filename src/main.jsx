c68fe44bd8abe4286a761dcca5f00f2e33f96532
Chetan Sharma <cs194555@gmail.com>
2024-05-14 12:41:48 +0530

    enhancement10

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId="676256167848-geg7fsv93inhda860s6c5k6d42g4qngd.apps.googleusercontent.com">
 
     <React.StrictMode>
-      <div >Hi</div>
       <App />
     </React.StrictMode>,
   </GoogleOAuthProvider>
