71e24f3996c3936cf7012104592a7c6fc5133a40
Chetan Sharma <cs194555@gmail.com>
2024-05-14 12:38:21 +0530

    enhancement9

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'../dist/',
+  base:'',
   build: {
     outDir: './dist',
     assetsDir: 'assets',
