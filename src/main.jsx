9b92517ad9ef2a482020ebcd16f7163b08909b0b
Chetan Sharma <cs194555@gmail.com>
2024-05-14 11:34:02 +0530

    enhancement2

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,11 @@ import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId="676256167848-geg7fsv93inhda860s6c5k6d42g4qngd.apps.googleusercontent.com">
+  // <GoogleOAuthProvider clientId="676256167848-geg7fsv93inhda860s6c5k6d42g4qngd.apps.googleusercontent.com">
 
     <React.StrictMode>
+      <div >Hi</div>
       <App />
     </React.StrictMode>,
-  </GoogleOAuthProvider>
+  // </GoogleOAuthProvider>
 )
